360f52312492939affdd5704f350e609e6a900e1
Alexey Lishuta <lishuta@rutorika.ru>
2023-06-02 23:06:52 +0300

    2 step

--- gendiff.js
+++ gendiff.js
@@ -7,7 +7,9 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
   .helpOption('-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();

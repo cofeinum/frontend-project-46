4591f7ee78f836bb4eede2ac442ddb251f6948a6
Alexey Lishuta <lishuta@rutorika.ru>
2023-08-26 00:59:32 +0300

    step 6

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,19 +1,21 @@
+import { readFileSync } from 'fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const readFile = (filename) => readFileSync(resolve(__dirname, '..', '__fixtures__', filename), 'utf-8');
 
 test('equal', () => {
   const pathJson1 = './__fixtures__/file1.json';
   const pathJson2 = './__fixtures__/file2.json';
   const pathYaml1 = './__fixtures__/file1.yaml';
   const pathYaml2 = './__fixtures__/file2.yaml';
-  const result = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+  const result = readFile('result.txt');
 
   expect(genDiff(pathJson1, pathJson2)).toEqual(result);
   expect(genDiff(pathYaml1, pathYaml2)).toEqual(result);
+  expect(genDiff(pathJson1, pathYaml2)).toEqual(result);
+  expect(genDiff(pathYaml1, pathJson2)).toEqual(result);
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,70 @@
 import readFile from './read-file.js';
 import getParse from './parser.js';
 
+const hasObject = (val) => (typeof val === 'object' && val !== null);
+
+const diff = (obj1, obj2, depth = 0) => {
+  const object1 = obj1;
+  const object2 = hasObject(obj2) ? obj2 : obj1;
+
+  const nextDepth = depth + 1;
+  const marginSymbol = ' ';
+  const marginLines = marginSymbol.repeat((depth + 1) * 4 - 2);
+  const marginLast = marginSymbol.repeat(depth * 4);
+
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  const result = [...keys1, ...keys2]
+    .filter((value, index, array) => array.indexOf(value) === index)
+    .sort()
+    .flatMap((key) => {
+      const items = [];
+      switch (true) {
+        // is only in the second `+ qwe: rty`
+        case !Object.hasOwn(object1, key):
+          items.push({
+            sign: '+',
+            val: hasObject(object2[key]) ? diff(object2[key], null, nextDepth) : object2[key],
+          });
+          break;
+        // is only in the first `- qwe: rty`
+        case !Object.hasOwn(object2, key):
+          items.push({
+            sign: '-',
+            val: hasObject(object1[key]) ? diff(object1[key], null, nextDepth) : object1[key],
+          });
+          break;
+        // are in both and values are objects
+        case hasObject(object1[key]) && hasObject(object2[key]):
+          items.push({
+            sign: ' ',
+            val: diff(object1[key], object2[key], nextDepth),
+          });
+          break;
+        // is in both and the values are the same (not objects!)
+        case object1[key] === object2[key]:
+          items.push({
+            sign: ' ',
+            val: object1[key],
+          });
+          break;
+        // exists in both objects and the values do NOT match
+        default:
+          items.push({
+            sign: '-',
+            val: hasObject(object1[key]) ? diff(object1[key], null, nextDepth) : object1[key],
+          }, {
+            sign: '+',
+            val: hasObject(object2[key]) ? diff(object2[key], null, nextDepth) : object2[key],
+          });
+      }
+      return items.map((item) => `${marginLines}${item.sign} ${key}: ${item.val}`);
+    });
+
+  return ['{', ...result, `${marginLast}}`].join('\n');
+};
+
 const genDiff = (path1, path2) => {
   const data1 = readFile(path1);
   const data2 = readFile(path2);
@@ -11,29 +75,7 @@ const genDiff = (path1, path2) => {
   const object1 = getParse(data1, format1);
   const object2 = getParse(data2, format2);
 
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-
-  const result = [...keys1, ...keys2]
-    .filter((value, index, array) => array.indexOf(value) === index)
-    .sort()
-    .flatMap((key) => {
-      if (!Object.hasOwn(object1, key)) {
-        return `+ ${key}: ${object2[key]}`;
-      }
-      if (!Object.hasOwn(object2, key)) {
-        return `- ${key}: ${object1[key]}`;
-      }
-      if (object1[key] === object2[key]) {
-        return `  ${key}: ${object1[key]}`;
-      }
-      return [
-        `- ${key}: ${object1[key]}`,
-        `+ ${key}: ${object2[key]}`,
-      ];
-    });
-
-  return `{\n  ${result.join('\n  ')}\n}`;
+  return diff(object1, object2);
 };
 
 export default genDiff;
